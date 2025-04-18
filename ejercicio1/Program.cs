103462ae6b6f460a57514478a2b09cb5df514d44
crazy <axelvasquez582@gmail.com>
2025-04-18 13:25:26 -0500

    agregando el resto de archivo del proyeco ejercicio1

---
+++ ejercicio1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //print variables
+            string name = "collete";
+            int age = 16;
+            Console.WriteLine($"mi nombre es {name} y tengo {age}");
+           
+
+
+
+
+
+        }
+    }
+}
