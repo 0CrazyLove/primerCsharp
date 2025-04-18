0b1281f06f6532cf3da6d9c4aa4bd363a26ca28a
crazy <axelvasquez582@gmail.com>
2025-04-18 16:24:20 -0500

    subiendo archivos del ejercicio5

---
+++ ejercicio5/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio5
+{
+    internal class Program
+    {
+            //functions
+
+        static string saludar(string name) //funcion que recibe un string y devuelve un string
+        {
+            return $"hola {name} eres un pendejo!";
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(saludar("miguel"));
+            
+
+
+        }
+    }
+}
