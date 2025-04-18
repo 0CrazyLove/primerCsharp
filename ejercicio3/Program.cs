26115fadbaaeefc0fc5a5cbcfed27a9d56bb20c5
crazy <axelvasquez582@gmail.com>
2025-04-18 14:20:35 -0500

    eliminacion de archivo corrupto

---
+++ ejercicio3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //bucle for && table multiplìcation
+
+            Console.Write("inserte un numero para generar la tabla del 1 al 10: ");
+            int number = int.Parse(Console.ReadLine()); //insertar el numero que quieres para la tabla
+            for (int i = 1; i <=10; i++)
+            {
+                int result = (number * i);
+                    
+                Console.WriteLine($"{number} * {i} = {result}");
+            }
+
+
+
+        }
+    }
+}
