b175b780547059f78846a6ed828c254a3b29544c
crazy <axelvasquez582@gmail.com>
2025-04-18 16:47:43 -0500

    subiendo archivos de ejercico6

---
+++ ejercico6/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercico6
+{
+    internal class Program
+    {
+        //function suma && resta
+
+        static string suma(int a , int b)
+        {
+            return $"la suma de {a} y de {b} es: {a + b}";
+        }  
+        static string resta(int a , int b)
+        {
+            return $"la resta de {a} y de {b} es: {a - b}";
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("elige una opcion: ");
+            Console.WriteLine("1. suma");
+            Console.WriteLine("2. resta");
+            int option = int.Parse(Console.ReadLine());
+
+            if(option != 1 && option != 2) {
+                Console.WriteLine("opcion no valida.");
+               Main(args);
+
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
