376ffd9e53cd88397d976d3dd2ee981915a3c4c0
crazy <axelvasquez582@gmail.com>
2025-04-18 13:42:50 -0500

    archivos del proyecto ejercico2

---
+++ ejercicio2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //conditinal if
+            Console.WriteLine("entraras al puti club furry gay trans xxx si tienes mas de 18 años.");
+
+            string person = "miguel";
+            int age = 18;
+
+            if(age < 18)
+            {
+                Console.Write($" no puedes pasar puto {person} ");
+            }
+            else
+            {
+                Console.Write($"disfruta (carita hot) ");
+            }
+
+
+
+        }
+    }
+}
