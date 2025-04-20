a86a094db4ff3d8799aabebfabedf8409375cca1
crazy <axelvasquez582@gmail.com>
2025-04-19 22:06:18 -0500

    subiendo archivos de ejercicio7

---
+++ ejercicio7/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio7
+{
+    internal class Program
+      
+
+    {
+
+        static void Main(string[] args)
+        {
+            //arrays     
+            string[] names = { "juan" , "miguel" , "andres" };
+            Console.WriteLine(names[1]);
+            //cambiando el valor del indice 0 del array
+            names[0]= "samandy";
+
+            Console.WriteLine(names[0]);
+
+            //usando la propiedad Length 
+            Console.WriteLine(names.Length);
+
+
+
+            string[] brawl;
+
+             brawl = new string[3];
+            brawl[0]= "pendejo";
+            brawl[2]= "tonto";
+            brawl[1]= "feo";
+            Console.WriteLine(brawl[2]);
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
