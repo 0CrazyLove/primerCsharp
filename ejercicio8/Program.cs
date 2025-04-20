40ad4b37916818b0eb53ca405c361dc10155c682
crazy <axelvasquez582@gmail.com>
2025-04-19 22:40:18 -0500

    subiendo los archivos de ejercico 8

---
+++ ejercicio8/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //utilizando un bucle para recorrer los elementos del array
+
+            //inciando el array
+            int[] age;
+
+            age = new int[10] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            for(int i = 0; i < age.Length; i++)
+            {
+                Console.WriteLine(age[i]);
+            }
+
+
+
+
+        }
+    }
+}
